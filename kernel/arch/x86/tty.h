af3edf82b399d3309ea0698b6afaed939c1d8307
verzogern <tone121@cox.net>
2017-03-18 16:01:17 -0700

    Added a prototype clear screen function.
    Also changed up some things

--- kernel/arch/x86/tty.h
+++ kernel/arch/x86/tty.h
@@ -11,5 +11,6 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
 void terminal_write(const char* data, size_t size, size_t format);
 void terminal_writestring(const char* data);
 void terminal_printf(const char* data, ...);
+void terminal_clearscreen();
 
 #endif
