c247a9ecac9ef18e35e6319a5240cae99b38faba
peany <tone121@cox.net>
2017-03-20 10:07:56 -0700

    Fixed up I/O inline assembly functions.
    Files changed:
    ioasm.c

--- kernel/arch/x86/ioasm.c
+++ kernel/arch/x86/ioasm.c
@@ -3,37 +3,37 @@
 // output stuff
 void outb(uint32_t addr, uint8_t val)
 {
-	asmv("outb %%al, %%ds" :: "d" (addr), "a" (val));
+	asmv("outb %0, %1" : : "a" (val), "Nd" (addr));
 }
 
 void outw(uint32_t addr, uint16_t val)
 {
-	asmv("outw %%al, %%ds" :: "d" (addr), "a" (val));
+	asmv("outw %0, %1" : : "a" (val), "Nd" (addr));
 }
 
 void outl(uint32_t addr, uint32_t val)
 {
-	asmv("outl %%al, %%ds" :: "d" (addr), "a" (val));
+	asmv("outl %0, %1" : : "a" (val), "Nd" (addr));
 }
 
 // input stuff
-uint8_t inb(uint32_t addr)
+uint8_t inb(uint32_t addr
 {
 	uint8_t r;
-	asmv("inb %%ds, %%al" : "=a" (r) : "d" (ad));
+	asmv("inb %1, %0" : "=a" (r) : "Nd" (addr));
 	return r;
 }
 
 uint16_t inw(uint32_t addr)
 {
 	int8_t r;
-	asmv("inb %%ds, %%al" : "=a" (r) : "d" (ad));
+	asmv("inw %1, %0" : "=a" (r) : "Nd" (addr));
 	return r;
-}
+
 
 uint32_t inl(uint32_t addr)
 {
 	uint8_t r;
-	asmv("inb %%ds, %%al" : "=a" (r) : "d" (ad));
+	asmv("inl %1, %0" : "=a" (r) : "Nd" (addr));
 	return r;
 }
