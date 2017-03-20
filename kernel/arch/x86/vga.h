5779e5453e3c9d5536c755e56984573b35e43911
verzogern <tone121@cox.net>
2017-03-19 20:29:01 -0700

    Made some fixes to various files.
    TODO: fix ioasm.c

--- kernel/arch/x86/vga.h
+++ kernel/arch/x86/vga.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+static const uint32_t VGA_WIDTH = 80;
+static const uint32_t VGA_HEIGHT = 25;
 static uint16_t* const VGA_TTY_ADDR = (uint16_t*) 0xB8000;
 
 enum vga_color {
--- kernel/libc/string.c
+++ kernel/libc/string.c
@@ -13,7 +13,7 @@ void* memset(void* s, int c, size_t n)
 	unsigned char* dst = (unsigned char*) s;
 	const unsigned char* src = (const unsigned char*) c;
 	
-	for(size_t i; i < n, i++)
+	for(size_t i = 0; i < n; i++)
 		dst[i] = src[i];
 	
 	return dst;
