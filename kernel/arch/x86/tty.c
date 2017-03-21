ef34373eae70307d8212954a7b9d2aa1b80f1898
verzogern <tone121@cox.net>
2017-03-20 19:30:23 -0700

    Added printf();
    Changed Files:
    arch\x86\tty.c
    libc\Makefile
    libc\stdlib.c
    libc\stdlib.h
    TODO.txt

--- kernel/arch/x86/tty.c
+++ kernel/arch/x86/tty.c
@@ -5,6 +5,7 @@
 #include <stdint.h>  // for integer types
 #include <stdarg.h>  // for ellipsis stuff
 #include <libc/string.h>
+#include <libc/stdlib.h>
  
 static size_t terminal_row;
 static size_t terminal_column;
@@ -61,17 +62,107 @@ void terminal_write(const char* data, size_t size, size_t format)
 		terminal_putchar(data[i], format);
 }
  
-void terminal_writestring(const char* data) 
+void terminal_print(const char* data) 
 {
 	terminal_write(data, strlen(data), 0);
 }
 
-void terminal_printf(const char* data, ...)
+void terminal_printf(const char* s, ...)
 {
-	va_list list;
-	va_start(list, data);
-	terminal_write(data, strlen(data), 1);
-	va_end(list);
+	va_list ap;
+
+	char buf[16];
+	int i, j, size, buflen, neg;
+
+	unsigned char c;
+	int ival;
+	unsigned int uival;
+
+	va_start(ap, s);
+
+	while ((c = *s++)) {
+		size = 0;
+		neg = 0;
+
+		if (c == 0)
+			break;
+		else if (c == '%') {
+			c = *s++;
+			if (c >= '0' && c <= '9') {
+				size = c - '0';
+				c = *s++;
+			}
+
+			if (c == 'd') {
+				ival = va_arg(ap, int);
+				if (ival < 0) {
+					uival = 0 - ival;
+					neg++;
+				} else
+					uival = ival;
+				itoa(buf, uival, 10);
+
+				buflen = strlen(buf);
+				if (buflen < size)
+					for (i = size, j = buflen; i >= 0;
+					     i--, j--)
+						buf[i] =
+						    (j >=
+						     0) ? buf[j] : '0';
+
+				if (neg)
+					terminal_printf("-%s", buf);
+				else
+					terminal_printf(buf);
+			}
+			 else if (c == 'u') {
+				uival = va_arg(ap, int);
+				itoa(buf, uival, 10);
+
+				buflen = strlen(buf);
+				if (buflen < size)
+					for (i = size, j = buflen; i >= 0;
+					     i--, j--)
+						buf[i] =
+						    (j >=
+						     0) ? buf[j] : '0';
+
+				terminal_printf(buf);
+			} else if (c == 'x' || c == 'X') {
+				uival = va_arg(ap, int);
+				itoa(buf, uival, 16);
+
+				buflen = strlen(buf);
+				if (buflen < size)
+					for (i = size, j = buflen; i >= 0;
+					     i--, j--)
+						buf[i] =
+						    (j >=
+						     0) ? buf[j] : '0';
+
+				terminal_printf("0x%s", buf);
+			} else if (c == 'p') {
+				uival = va_arg(ap, int);
+				itoa(buf, uival, 16);
+				size = 8;
+
+				buflen = strlen(buf);
+				if (buflen < size)
+					for (i = size, j = buflen; i >= 0;
+					     i--, j--)
+						buf[i] =
+						    (j >=
+						     0) ? buf[j] : '0';
+
+				terminal_printf("0x%s", buf);
+			} else if (c == 's') {
+				terminal_printf((char *) va_arg(ap, int));
+			} 
+		} else
+			terminal_putchar(c, 1);
+	}
+
+	return;
 }
 
 void terminal_clearscreen()
---
+++ kernel/libc/stdlib.h
@@ -0,0 +1,6 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+
+void itoa(char *buf, unsigned long int n, int base);
+
+#endif
