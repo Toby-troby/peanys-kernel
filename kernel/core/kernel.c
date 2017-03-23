73dfc9caacb11c6167a7abb64d4ff3abd3fa00b7
verzogern <tone121@cox.net>
2017-03-23 16:47:11 -0700

    added func in kernel.c that sets up basic things.

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -1,15 +1,20 @@
 #include <core/os.h>
 #include <core/io.h>
 
+static void basic_setup(void)
+{
+	enable_a20();
+	terminal_initialize();
+}
+
 void kernel_main(void) 
 {
-	terminal_initialize();
+	basic_setup();
+	
 	kprintf("Peany's Kernel\n");
 	kprintf("Version: %d.%d.%d\n",
 		KERNEL_VER_MAJOR,
 		KERNEL_VER_MINOR,
 		KERNEL_VER_PATCH
 	);
-	
-	return;
 }
