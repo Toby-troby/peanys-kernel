de59f92aa496383271c054039ea5fdb16d3437bc
verzogern <tone121@cox.net>
2017-03-25 15:43:38 -0700

    Added struct for multi-boot information.

--- kernel/arch/x86/boot.h
+++ kernel/arch/x86/boot.h
@@ -56,4 +56,28 @@ inline void initregs(struct biosregs *regs)
 	memset(regs, 0, sizeof(*regs));
 }
 
+struct mbi {
+	u32 flags;
+	u32 mem_lower;
+	u32 mem_upper;
+	u32 boot_device;
+	u32 cmdline;
+	u32 mods_count;
+	u32 mods_addr;
+	u32 drives_length;
+	u32 drives_addr;
+	u32 mmap_length;
+	u32 mmap_addr;
+	u32 config_table;
+	u32 boot_loader_name;
+	u32 apm_table;
+	u32 vbe_control_info;
+	u32 vbe_mode_info;
+	u32 vbe_mode;
+	u32 vbe_interface_seg;
+	u32 vbe_interface_off;
+	u32 vbe_interface_len;
+};
+
+
 #endif
