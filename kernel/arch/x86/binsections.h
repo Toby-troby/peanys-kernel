58f75f97ef27676c7019ef88b7d7936416d672f9
peany <tone121@cox.net>
2017-03-29 10:08:33 -0700

    Added binsections.h
    This file tells the kernel where it's sections are in it's binary.

---
+++ kernel/arch/x86/binsections.h
@@ -0,0 +1,19 @@
+#ifndef x86_BINSECTIONS_H
+#define x86_BINSECTIONS_H
+
+/* Access these with & */
+
+extern unsigned int s_text;
+extern unsigned int e_text;
+
+extern unsigned int s_rodata;
+extern unsigned int e_rodata;
+
+extern unsigned int s_data;
+extern unsigned int e_data;
+
+extern unsigned int s_bss;
+extern unsigned int e_bss;
+extern unsigned int e_kernel;
+
+#endif /* x86_BINSECTIONS_H */
