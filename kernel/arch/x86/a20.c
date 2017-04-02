48cfe912fbe4065044db210b0e755f319f8c9cd0
verzogern <tone121@cox.net>
2017-04-02 02:36:42 -0700

    changed a20 things

--- kernel/arch/x86/a20.c
+++ kernel/arch/x86/a20.c
@@ -1,13 +1,15 @@
 #include "boot.h"
 
+#define FAST_A20_GATE 0x92
+
 static void enable_a20_fast(void)
 {
 	u8 port_a;
 	
-	port_a = inb(0x92);
+	port_a = inb(FAST_A20_GATE);
 	port_a |= 0x02;
 	port_a &= ~0x01;
-	outb(port_a, 0x92);
+	outb(port_a, FAST_A20_GATE);
 }
 
 void enable_a20(void)
