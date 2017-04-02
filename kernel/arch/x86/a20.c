644038710295b87f6191ed884786eb45074a40b2
verzogern <tone121@cox.net>
2017-04-02 02:44:51 -0700

    Correctly initialized A20 line.

--- kernel/arch/x86/a20.c
+++ kernel/arch/x86/a20.c
@@ -4,11 +4,8 @@
 
 static void enable_a20_fast(void)
 {
-	u8 port_a;
-	
-	port_a = inb(FAST_A20_GATE);
+	u8 port_a = inb(FAST_A20_GATE);	
 	port_a |= 0x02;
-	port_a &= ~0x01;
 	outb(port_a, FAST_A20_GATE);
 }
 
