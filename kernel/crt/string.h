63da29cf25464162b62b7d518ca8ac691376f1e7
Toby-troby <lucario22200@gmail.com>
2017-08-02 20:15:15 -0700

    Update string.h
    add memcmp and memmove

--- kernel/crt/string.h
+++ kernel/crt/string.h
@@ -7,5 +7,8 @@ size_t strlen(const char* str);
 void memset(void* s, int c, size_t n);
 void memcpy(void *dest, void *src, size_t n);
 
+int memcmp(const void *ptr1, const void *ptr2, size_t bytes);
+void *memmove(void *dest, const void *src, size_t bytes);
+
 #endif /* CRT_STRING_H */
 
